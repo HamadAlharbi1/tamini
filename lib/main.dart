139d671de975893036ad47ff888bf2528129d6a6
hamad alharbi <hamad11131@gmail.com>
2023-09-27 17:24:03 +0300

    Integrated Firebase into Tamini app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
-void main() {
-  /**
-   *  main app
-   */
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:tamini_app/app.dart';
+import 'package:tamini_app/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    ),
+  );
 }
---
+++ lib/pages/homepage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('تأميني')),
+      ),
+      body: const Center(),
+    );
+  }
+}
