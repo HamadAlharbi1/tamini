7e5f12d19c78c223529e3d8353f16f01eb08984b
Bassam A <mutairibassam@gmail.com>
2023-09-29 10:37:14 +0300

    fixes: hide firebase configs

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'package:tamini_app/app.dart';
 import 'package:tamini_app/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await dotenv.load();
+  var config = await Firebase.initializeApp(
+      options: DefaultFirebaseOptions().currentPlatform);
+  print(config);
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
