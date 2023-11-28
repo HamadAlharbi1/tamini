786cabe9d1a64bb3b0c01d0fe6bead15ba4f7f62
Hamad Alharbi <hamad11131@gmail.com>
2023-11-28 21:31:34 +0300

    fix app icon size ,update  app name to Tameenk

--- lib/components/quotations/quotation_service_cost.dart
+++ lib/components/quotations/quotation_service_cost.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:localization/localization.dart';
+import 'package:provider/provider.dart';
 import 'package:tamini_app/components/constants.dart';
+import 'package:tamini_app/provider/language_provider.dart';
 
 class QuotationServiceCost extends StatefulWidget {
   const QuotationServiceCost({Key? key}) : super(key: key);
@@ -14,6 +16,7 @@ class _QuotationServiceCostState extends State<QuotationServiceCost> {
 
   @override
   Widget build(BuildContext context) {
+    Provider.of<LanguageProvider>(context); // this is added since the language could changes on profile_page
     return ExpansionPanelList(
       expansionCallback: (int index, bool isExpanded) {
         setState(() {
@@ -35,7 +38,7 @@ class _QuotationServiceCostState extends State<QuotationServiceCost> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  "${Constants.quotationCost} ${"S.R".i18n()} ${"after_complete".i18n()}",
+                  "${Constants.quotationCost} ${"S_R".i18n()} ${"after_complete".i18n()}",
                   style: Theme.of(context).textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold),
                 ),
               ],
